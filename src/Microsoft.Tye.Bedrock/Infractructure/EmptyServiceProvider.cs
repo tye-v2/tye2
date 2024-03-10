9d1119c24c2d509b77c4f304659e4169c06d07c4
drakon660 <drakon660@gmail.com>
2024-03-10 09:53:04 +0100

    bedrock merged + nuget package update

---
+++ src/Microsoft.Tye.Bedrock/Infractructure/EmptyServiceProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Microsoft.Tye.Proxy;
+
+internal class EmptyServiceProvider : IServiceProvider
+{
+    public static IServiceProvider Instance { get; } = new EmptyServiceProvider();
+
+    public object GetService(Type serviceType) => null;
+}
